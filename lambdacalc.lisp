4e963d00a7788c70a4b7332c9b8af1989ef1ec32
Joshua Chubb <chubb.jp@gmail.com>
2011-10-29 22:08:56 +1100

    some changes on the calculator

--- lambdacalc.lisp
+++ lambdacalc.lisp
@@ -11,26 +11,34 @@
 ;; 
 ;; ========================================
 
+;; ================= ;;
+;;; Church numerals  ;;
+;; ================= ;;
+
+;; Zero
 ;; \fx. x
 (defvar zero
 	   (lambda (f)
 	     (lambda (x) x)))
 
+;; Succ
 ;; \nfx. f (n f x)
 (defun succ(n)
 	   (lambda (f)
 	     (lambda (x) (funcall f (funcall (funcall n f) x)))))
 
+;; Plus
 ;; \mnfx. m f (n f x)
 (defun plus (m n)
 	   (lambda (f)
 	     (lambda (x) (funcall (funcall m f) (funcall (funcall n f) x))))) 
 
+;; Mult
 ;; \mnf. n (m f)
 (defun mult(m n)
 	   (lambda (f)
 	     (funcall n (funcall m f))))
-
+;; Pred
 ;; \nfx. n (\gh. h (g f)) (\u. x) (\u. u)
 (defun pred (n)
 	   (lambda (f)
@@ -40,14 +48,48 @@
 						(funcall h (funcall g f)))))
 				 (lambda (u) x))
 			(lambda (u)u)))))
-
+;; Sub
 ;; \mn. (n pred) m 
 (defun sub(m n)
 	   (funcall (funcall n #'pred) m))
 
+;; Exp
 ;; \mn. n m
 (defun cexp(m n)
 	   (funcall n m))
+
+;; ===================== ;;
+;;; Propositional Logic  ;;
+;; ===================== ;;
+
+;; use zero for false, might as well...
+
+;; T
+;; \xy. x
+(defvar ctrue
+  (lambda(x)
+    (lambda(y) x)))
+
+;; if-then-else
+;; \pqr. p q r
+(defun if-then-else(p q r)
+  (funcall (funcall p q) r))
+
+;; and
+;; \pq. if-then-else p q 0
+(defun cand(p q)
+  (if-then-else p q zero))
+
+;; or
+;; \pq. if-then-else p true q
+(defun cor(p q)
+  (if-then-else p ctrue q))
+
+;; not
+;; \pxy. pyx
+(defun cnot(p)
+  (lambda (x)
+    (lambda (y) (funcall (funcall p x) y))))
 
 ;; isZero
 ;; \n. n (\x. F) T
